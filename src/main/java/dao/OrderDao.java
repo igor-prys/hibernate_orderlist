49cff0c2430c734d760525ca562c5be0a5124009
igor-prys <photo.prys@gmail.com>
2023-04-12 22:51:40 +0300

    improve methodDelete remove comments

--- src/main/java/dao/OrderDao.java
+++ src/main/java/dao/OrderDao.java
@@ -6,9 +6,13 @@ import pojo.Order;
 import java.util.List;
 
 public interface OrderDao {
-   void add(Order order);
+    void add(Order order);
+
     Order find(Long id);
+
     List<Order> findAll();
+
     void update(Order order);
+
     boolean delete(Long id);
 }
